6bd74ba174d80fe2bff7a13a6d036d606525cf8f
Eugene Yokota <eed3si9n@gmail.com>
2016-04-07 14:52:54 -0400

    sbt-houserules

--- core/src/io.scala
+++ core/src/io.scala
@@ -1,91 +1,91 @@
 package sbinary;
 
 case class EOF() extends Exception;
-object EOF{
+object EOF {
   def eof = throw new EOF;
 }
 
 import EOF.eof
 
-trait Input{
+trait Input {
 
   /**
    * Read a byte. If we have come to the end of the stream, throws EOF.
    */
-  def readByte : Byte;
+  def readByte: Byte;
 
-  def readTo(target : Array[Byte], offset : Int, length : Int) : Int = {
+  def readTo(target: Array[Byte], offset: Int, length: Int): Int = {
     var i = 0;
 
-    try{
-      while (i < length){
+    try {
+      while (i < length) {
         target(offset + i) = readByte;
         i += 1;
       }
     } catch {
-      case EOF() =>;
+      case EOF() => ;
     }
 
     i;
   }
 
-  def readTo(target : Array[Byte]) : Int = readTo(target, 0, target.length);
+  def readTo(target: Array[Byte]): Int = readTo(target, 0, target.length);
 
-  def readFully(target : Array[Byte], offset : Int, length : Int){
+  def readFully(target: Array[Byte], offset: Int, length: Int) {
     var bytesRead = 0;
 
-    while(bytesRead < length){
+    while (bytesRead < length) {
       val newRead = readTo(target, offset + bytesRead, length - bytesRead);
       if (newRead <= 0) eof;
       bytesRead += newRead;
     }
   }
 
-  def readFully(target : Array[Byte]){
+  def readFully(target: Array[Byte]) {
     readFully(target, 0, target.length);
-  } 
+  }
 }
 
-trait Output{
-  def writeByte(value : Byte);
+trait Output {
+  def writeByte(value: Byte);
 
-  def writeAll(source : Array[Byte], offset : Int, length : Int) {
+  def writeAll(source: Array[Byte], offset: Int, length: Int) {
     var i = 0;
 
-    while(i < length){
+    while (i < length) {
       writeByte(source(offset + i));
       i += 1;
     }
   }
 
-  def writeAll(source : Array[Byte]){
+  def writeAll(source: Array[Byte]) {
     writeAll(source, 0, source.length);
   }
 }
 
 import java.io._;
 
-class JavaInput(in : InputStream) extends Input{
+class JavaInput(in: InputStream) extends Input {
   def readByte = in.read() match {
-    case x if x < 0 => eof; 
-    case x => x.toByte;
+    case x if x < 0 => eof;
+    case x          => x.toByte;
   }
 
-  override def readTo(target : Array[Byte], offset : Int, length : Int) : Int = 
+  override def readTo(target: Array[Byte], offset: Int, length: Int): Int =
     in.read(target, offset, length) match {
       case x if x < 0 => eof;
-      case x => x;
+      case x          => x;
     }
 }
 
-class JavaOutput(out : OutputStream) extends Output{
-  def writeByte(value : Byte) = out.write(value);
-  override def writeAll(source : Array[Byte], offset : Int, length : Int) = out.write(source, offset, length);
+class JavaOutput(out: OutputStream) extends Output {
+  def writeByte(value: Byte) = out.write(value);
+  override def writeAll(source: Array[Byte], offset: Int, length: Int) = out.write(source, offset, length);
 }
 
 object Input {
-  implicit def javaInputToInput(x : InputStream): JavaInput = new JavaInput(x)
+  implicit def javaInputToInput(x: InputStream): JavaInput = new JavaInput(x)
 }
 object Output {
-  implicit def javaOutputToOutput(x : OutputStream): JavaOutput = new JavaOutput(x);
+  implicit def javaOutputToOutput(x: OutputStream): JavaOutput = new JavaOutput(x);
 }
--- core/src/operations.scala
+++ core/src/operations.scala
@@ -1,50 +1,50 @@
 
 package sbinary;
 
-object Operations{
-  import java.io.{BufferedInputStream, BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream, File, FileInputStream, FileOutputStream};
+object Operations {
+  import java.io.{ BufferedInputStream, BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream, File, FileInputStream, FileOutputStream };
 
-  def format[T](implicit fm : Format[T]) = fm;
+  def format[T](implicit fm: Format[T]) = fm;
 
-  def read[T](in : Input)(implicit reader : Reads[T]) = reader.reads(in);
-  def write[T](out : Output, value : T)(implicit writer : Writes[T]) = writer.writes(out, value);
-
+  def read[T](in: Input)(implicit reader: Reads[T]) = reader.reads(in);
+  def write[T](out: Output, value: T)(implicit writer: Writes[T]) = writer.writes(out, value);
 
   /**
    * Get the serialized value of this class as a byte array.
    */
-  def toByteArray[T](t : T)(implicit bin : Writes[T]) : Array[Byte] = {
+  def toByteArray[T](t: T)(implicit bin: Writes[T]): Array[Byte] = {
     val target = new ByteArrayOutputStream();
     bin.writes(target, t);
-    target.toByteArray(); 
+    target.toByteArray();
   }
- 
+
   /**
    * Read a value from the byte array. Anything past the end of the value will be
    * ignored.
-   */ 
-  def fromByteArray[T](array : Array[Byte])(implicit bin : Reads[T]) = read[T](new ByteArrayInputStream(array));
+   */
+  def fromByteArray[T](array: Array[Byte])(implicit bin: Reads[T]) = read[T](new ByteArrayInputStream(array));
 
-  /** 
+  /**
    * Convenience method for writing binary data to a file.
    */
-  def toFile[T](t : T)(file : File)(implicit bin : Writes[T]) = {
+  def toFile[T](t: T)(file: File)(implicit bin: Writes[T]) = {
     val out = new BufferedOutputStream(new FileOutputStream(file));
-    try{
-      out.write(toByteArray(t));}
-    finally{
-      out.close(); }
+    try {
+      out.write(toByteArray(t));
+    } finally {
+      out.close();
+    }
   }
 
-  /** 
+  /**
    * Convenience method for reading binary data from a file.
    */
-  def fromFile[T](file : File)(implicit bin : Reads[T]) = {
+  def fromFile[T](file: File)(implicit bin: Reads[T]) = {
     val in = new BufferedInputStream(new FileInputStream(file))
-    try{
+    try {
       read[T](in)
-    } finally{
-      in.close(); 
+    } finally {
+      in.close();
     }
   }
 }
