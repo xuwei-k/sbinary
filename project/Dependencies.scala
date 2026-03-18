85e94939288da34a8fe07564d37b611e924a84b6
xuwei-k <6b656e6a69@gmail.com>
2026-03-18 14:28:27 +0900

    Update scala versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,9 +2,9 @@ import sbt._
 import Keys._
 
 object Dependencies {
-  val scala212 = "2.12.17"
-  val scala213 = "2.13.10"
-  val scala3 = "3.2.1"
+  val scala212 = "2.12.21"
+  val scala213 = "2.13.18"
+  val scala3 = "3.3.7"
 
   val scalacheck = Def.setting {
     "org.scalacheck" %% "scalacheck" % "1.15.4"
